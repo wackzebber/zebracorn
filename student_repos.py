1112c3fee7a036017579405bd28bb296730b1636
gabalafou <gabriel@fouasnon.com>
2023-12-09 15:18:32 -0500

    Create student_repos.py

---
+++ student_repos.py
@@ -0,0 +1,44 @@
+# This file exports a single variable, `student_repos`, which is 
+# a dictionary that maps students' first names to their repo for 
+# the class assignments.
+
+# Format for dictionary entries:
+#
+#    "First name": "<github username>/<repo name>"
+#
+# Example (if the teacher were a student): 
+# 
+#    "Gabriel": "gabalafou/cswinter"
+#
+student_repos = {
+    "Arthur": "",
+    "Asante": "",
+    "Carla": "",
+    "Clara": "",
+    "Daivyon": "",
+    "Daniel": "",
+    "Julia": "",
+    "Kritika": "",
+    "Leo": "",
+    "Lola": "",
+    "Michael": "",
+    "Ryder": "",
+    "Tiyanu": "",
+    "Varun": "",
+    "Will": "",
+    "Zack": "",
+    "Anabelle": "",
+    "Benjamin": "",
+    "Callum": "",
+    "Dilzafer": "",
+    "Edward": "",
+    "Emilia": "",
+    "Grace": "",
+    "Jacob": "",
+    "James": "",
+    "JR": "",
+    "Laurie": "",
+    "Luisa": "",
+    "Newton": "",
+    "Jason": "",
+}
