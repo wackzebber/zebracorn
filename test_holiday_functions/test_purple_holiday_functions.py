570666be576e5c1ee3872f35606f5ae56a257ceb
gabalafou <gabriel@fouasnon.com>
2023-12-09 20:11:15 -0500

    Create test_purple_holiday_functions.py

---
+++ test_holiday_functions/test_purple_holiday_functions.py
@@ -0,0 +1,69 @@
+# First read the instructions in the README.md file.
+#
+# Then if you are in the PURPLE section, find your name below and add two test 
+# cases right below your name to this file, patterning your test code after
+# the teacher's.
+#
+# IMPORTANT: Please only use two lines immediately below your name. Do not add lines. 
+# Do not use more than two lines. Otherwise, you may cause merge conflicts with
+# the other students who will also be commiting changes to this file.
+
+# Mr. Fouasnon
+assert home_for_the_holidays("Fouasnon") # since "Fouasnon" is between 5 and 13 letters long and "O" does not come before "F"
+assert not brings_figgy_pudding(5, 8, 2, 0) # even though 0 quizzes means the student is in the holiday spirit, a 5th form with 8 demerits is not nice, and so does not bring figgy pudding
+
+# Anabelle
+
+
+
+# Benjamin
+
+
+
+# Callum
+
+
+
+# Dilzafer
+
+
+
+# Edward
+
+
+
+# Emilia
+
+
+
+# Grace
+
+
+
+# Jacob
+
+
+
+# James
+
+
+
+# JR
+
+
+
+# Laurie
+
+
+
+# Luisa
+
+
+
+# Newton
+
+
+
+# Jason
+
+
